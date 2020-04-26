811fff3bcecf2b839ededfb73331002075cba520
alaa deego <alaadeego@gmail.comgit>
2020-04-27 01:47:47 +0200

    Initial Commit

---
+++ tryLearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryLearn
+//
+//  Created by  alaa alrayes on 4/27/20.
+//  Copyright © 2020 alrayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
